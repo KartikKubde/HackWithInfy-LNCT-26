e86529c212f64e6a9433f5170340217556cdf785
KartikKubde <152711515+KartikKubde@users.noreply.github.com>
2026-03-21 23:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (93.57%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack = []
+        last_index = [0]*26
+        seen = set()
+
+        for i in range(len(s)):                     # Here we are creating last_index array
+            ch = s[i]
+            last_index[ord(ch) - ord('a')] = i
+
+        for i in range(len(s)):
+            if s[i] not in seen:
+                while(stack and ord(stack[-1]) > ord(s[i]) and i < last_index[ord(stack[-1]) - ord('a')]):
+                    seen.remove(stack.pop())
+                stack.append(s[i])
+                seen.add(s[i])
+
+        return "".join(stack)
